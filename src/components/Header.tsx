f58a466707364cb5435dd4bc0eaa1b1742fd166e
Arsenic <russo03pac@hotmail.com>
2025-09-17 18:53:43 +0200

    [FE][Header.tsx] Creation of header and implemented color switching (dark/light mode)

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { Box, Link, Flex } from "@chakra-ui/react"
+import { ColorModeButton, useColorModeValue } from "./ui/color-mode"
+
+const Header = () => {
+  return (
+    <Flex p="4" bg={useColorModeValue('gray.100', 'gray.700')} justifyContent={'space-between'}>
+        <Flex gap="4" w={'100%'} justifyContent={'flex-start'}>
+            <Link href="/" >
+                Home
+            </Link>
+            <Link href="/store">
+                Store
+            </Link>
+            <Link href="/cart">
+                Cart
+            </Link>
+        </Flex>
+            <Box css={{justifySelf: 'flex-end'}}>
+                <ColorModeButton />
+            </Box>
+    </Flex>
+  )
+}
+
+export default Header
