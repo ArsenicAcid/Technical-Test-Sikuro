393eeb5dd2cbba8e32b57c1a17c4a5e1286e67d2
Arsenic <russo03pac@hotmail.com>
2025-09-17 18:03:53 +0200

    [BE/FE][App.tsx, main.tsx] Moved Chakra Provider to main file, implemented cart route and header component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Routes, Route } from "react-router-dom"
-import { Provider } from "./components/ui/provider"
 import { Box } from "@chakra-ui/react"
-import { Home } from "./pages/Home"
-import { Store } from "./pages/Store"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Store from "./pages/Store"
+import Cart from "./pages/Cart"
 
 
 function App() {
-return (
-  <Provider>
-    <Box mb="10">
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-        </Routes>
-    </Box>
-  </Provider>
-  )
-}
+  return (
+    <>
+      <Header />
+      <Box mb="10">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/cart" element={<Cart />} />
+          </Routes>
+      </Box>
+    </>
+    )
+  }
 export default App
