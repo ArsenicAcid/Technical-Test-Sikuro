a11e2916ccac4ef3b645cd8d3c2835d747845bbf
Manuel <russo03pac@hotmail.com>
2025-09-18 17:21:40 +0200

    [FE][Header.tsx] Implementation of cart icons, storename/logo in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { Box, Link, Flex, Stack } from "@chakra-ui/react"
 import { ColorModeButton, useColorModeValue } from "../ui/color-mode"
+import { ShoppingCartIconBlack, ShoppingCartIconWhite } from "../../styles/icons"
 
 const Header = () => {
   return (
@@ -10,14 +11,32 @@ const Header = () => {
       justifyContent={"space-between"}
       shadow={"sm"}
     >
-      <Stack gap="4" direction={"row"} w={"100%"} justifyContent={"flex-start"}>
+      <Stack
+        gap="4"
+        direction={"row"}
+        w={"100%"}
+        justifyContent={"flex-start"}
+        me={"auto"}
+      >
+        <Link
+          textDecor={"none"}
+          href="/"
+          fontSize={"2xl"}
+          fontWeight={"bold"}
+          borderRight={"1px solid lightgray"}
+          pr="4"
+        >
+          Rushop
+        </Link>
         <Link href="/">Home</Link>
         <Link href="/store">Store</Link>
-        <Link href="/cart">Cart</Link>
       </Stack>
-      <Box css={{ justifySelf: "flex-end" }}>
+      <Flex gap="2" alignItems={"center"}>
         <ColorModeButton />
-      </Box>
+        <Box w="1.4rem" h="1.4rem">
+        {useColorModeValue(<ShoppingCartIconBlack />, <ShoppingCartIconWhite />)}
+        </Box>
+      </Flex>
     </Flex>
   )
 }
