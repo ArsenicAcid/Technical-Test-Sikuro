d91f259e222b0e231a188befd045a8846feb3ced
Arsenic <russo03pac@hotmail.com>
2025-09-18 23:02:38 +0200

    [BE][Multiple Files] Linted and moved icons.tsx/theme.tsx to assets/ folder

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -3,7 +3,7 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode"
 import { ThemeProvider } from "next-themes"
-import { system } from "../../styles/theme"
+import { system } from "../../assets/styles/theme"
 
 export function Provider(props: ColorModeProviderProps) {
   return (
