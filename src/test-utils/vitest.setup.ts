c284ed37e61e21d7cdf6145a9d63fbb8e390377a
Manuel <russo03pac@hotmail.com>
2025-09-18 12:59:14 +0200

    [BE][Configuration files] Implementation of paths, files and configurations for Vitest tests

---
+++ src/test-utils/vitest.setup.ts
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom/vitest'
+import { cleanup } from '@testing-library/react'
+
+const matchMediaMock = vi.fn((query: unknown) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}));
+vi.stubGlobal("matchMedia", matchMediaMock);
+
+afterEach(() => {
+  cleanup()
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test-utils/vitest.setup.ts'],
+    css: true,
+    testTimeout: 5000,
+    reporters: ['verbose']
+  }
 })
