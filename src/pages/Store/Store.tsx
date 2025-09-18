958ba2900d4554893da805da034462c29fb577a8
Arsenic <russo03pac@hotmail.com>
2025-09-19 00:09:40 +0200

    [FE][Store.tsx] Basic store layout to display grid of products from provided Endpoint

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,5 +1,26 @@
+import ProductCard from "../../components/ProductCard/ProductCard"
+import { useProducts } from "../../hooks/fetchProducts/fetchProducts"
+import { Grid } from "@chakra-ui/react"
+
 const Store = () => {
-  return <div>Store</div>
+  const { products, isFetching, error } = useProducts()
+  if (isFetching) return <div>Loading...</div>
+  if (error) return <div>Error: {error}</div>
+  return (
+    <Grid
+      templateColumns={{
+        lg: "repeat(3, 1fr)",
+        md: "repeat(2, 1fr)",
+        sm: "repeat(1, 1fr)",
+      }}
+      gap={6}
+      p={8}
+    >
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </Grid>
+  )
 }
 
 export default Store
