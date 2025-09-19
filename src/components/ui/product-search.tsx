6e51deb604d7ce096173f95bd27ab073d0eb6a9a
Manuel <russo03pac@hotmail.com>
2025-09-19 14:45:53 +0200

    [FE/BE][product-search.tsx] Creation of product-search.tsx component for dynamic filtering and product searching

---
+++ src/components/ui/product-search.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { Flex, Portal, Select, createListCollection } from "@chakra-ui/react"
+
+type ProductSearchProps = {
+  searchQuery: string
+  setSearchQuery: (query: string) => void
+  selectedCategory: string
+  setSelectedCategory: (category: string) => void
+  categories: string[]
+}
+
+const ProductSearch = ({
+  searchQuery,
+  setSearchQuery,
+  selectedCategory,
+  setSelectedCategory,
+  categories,
+}: ProductSearchProps) => {
+  const categoryCollection = createListCollection({
+    items: categories.map((cat) => ({
+      label: cat,
+      value: cat,
+    })),
+  })
+
+  return (
+    <Flex
+      gap={4}
+      direction={{ base: "column", md: "row" }}
+      justify="center"
+      alignItems={"flex-end"}
+      my={4}
+      wrap="nowrap"
+    >
+      <Select.Root
+        collection={categoryCollection}
+        width="30%"
+        value={[selectedCategory]}
+        onValueChange={(e) => setSelectedCategory(e.value[0])}
+      >
+        <Select.HiddenSelect />
+        <Select.Label>Filter by Category</Select.Label>
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Select category" />
+          </Select.Trigger>
+          <Select.IndicatorGroup>
+            <Select.Indicator />
+          </Select.IndicatorGroup>
+        </Select.Control>
+
+        <Portal>
+          <Select.Positioner>
+            <Select.Content>
+              {categoryCollection.items.map((item) => (
+                <Select.Item
+                  key={item.value}
+                  item={item}
+                  textTransform={"capitalize"}
+                >
+                  {item.label}
+                  <Select.ItemIndicator />
+                </Select.Item>
+              ))}
+            </Select.Content>
+          </Select.Positioner>
+        </Portal>
+      </Select.Root>
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        style={{
+          border: "1px solid lightgray",
+          borderRadius: "4px",
+          padding: "6px",
+          width: "70%",
+          height: "40px",
+        }}
+      />
+    </Flex>
+  )
+}
+
+export default ProductSearch
